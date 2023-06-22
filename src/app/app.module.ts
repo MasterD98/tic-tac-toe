1fdd24900d3b311bf4183c335b15524210605902
Dasith Rathnasinghe <dasithrathnasinghe@gmail.com>
2023-06-23 05:05:56 +0530

    basic logic done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SquareComponent } from './square/square.component';
-import { BoardComponent } from './board/board.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { SquareComponent } from './square/square.component';
+import { BoardComponent } from './board/board.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SquareComponent,
-    BoardComponent
+    BoardComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -2,16 +2,11 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
-  template: `
-    <button nbButton *ngIf="!value">{{ value }}</button>
-    <button nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
-    <button nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
-  `,
-  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
+  templateUrl: './square.component.html',
+  styleUrls: ['./square.component.scss']
 })
-export class SquareComponent  {
+export class SquareComponent {
 
   @Input()
-  value!: string| 'X' | 'O' ;
-
+  value!: 'X' | 'O';
 }
